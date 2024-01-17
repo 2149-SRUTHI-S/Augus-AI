0fd3496f6689fea283978364cab07fe18d4f1e02
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2024-01-17 19:59:49 +0530

    Update main.py

--- Augus-AI/main.py
+++ Augus-AI/main.py
@@ -1,7 +1,7 @@
 # AUGUS AI - AI Based Desktop Assistant for Blind
 # By Sruthi Sivasankararaj    
   
-import speech_recognition as sr  
+import speech_recognition as sr    
 import os
 import pyttsx3
 
